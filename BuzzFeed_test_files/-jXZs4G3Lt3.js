095a963602059473bfda6e511a51da44c4ad35aa
Theo Browne <theo@theobrowne.com>
2014-11-16 02:30:18 -0500

    Broken test files removed

--- BuzzFeed_test_files/-jXZs4G3Lt3.js
+++
@@ -1,5 +0,0 @@
-/*!CK:1220026828!*//*1415591819,*/
-
-if (self.CavalryLogger) { CavalryLogger.start_js(["v61ok"]); }
-
-__d("ChatTabTypeaheadDataSource",["MercuryConstants","MercuryTypeaheadDataSource"],function(a,b,c,d,e,f,g,h){for(var i in h)if(h.hasOwnProperty(i))k[i]=h[i];var j=h===null?null:h.prototype;k.prototype=Object.create(j);k.prototype.constructor=k;k.__superConstructor__=h;function k(l){"use strict";l=l||{};l.maxResults=g.MercuryTypeaheadConstants.COMPOSER_CHATTAB_MAX;h.call(this,l);this.$ChatTabTypeaheadDataSource0=true;}k.prototype.buildData=function(l){"use strict";var m=l;if(!this.$ChatTabTypeaheadDataSource0)m=l.filter(function(n){var o=j.getEntry.call(this,n);return o.render_type!=g.MercuryTypeaheadConstants.THREAD_TYPE;},this);return j.buildData.call(this,m);};k.prototype.setShowThreads=function(l){"use strict";this.$ChatTabTypeaheadDataSource0=l;};e.exports=k;},null);
--- BuzzFeed_test_files/eeYMrkRarrT.js
+++
@@ -1,6 +0,0 @@
-/*!CK:1791522737!*//*1414989707,*/
-
-if (self.CavalryLogger) { CavalryLogger.start_js(["\/2smt"]); }
-
-__d("CanvasResizer",["createArrayFrom","CSS","DOMEventListener","Vector"],function(a,b,c,d,e,f,g,h,i,j){var k;function l(){var n,o=document.documentElement;if(window.innerHeight){n=window.innerHeight;}else if(o&&o.clientHeight){n=o.clientHeight;}else n=document.body.clientHeight;for(var p=0;p<k.length;p++){var q=k[p];if(!h.hasClass(q,'noresize')){var r=j.getElementPosition(q,'document').y,s=n-r;q.style.height=s/(k.length-p)+'px';}}}i.add(window,'resize',l);var m={smartSizingFrameAdded:function(){k=[];var n=g(document.getElementsByTagName('iframe'));n.forEach(function(o){if(h.hasClass(o,'smart_sizing_iframe')&&!h.hasClass(o,'noresize')){h.removeClass(o,'canvas_iframe_util');k.push(o);}});l();}};e.exports=m;},null);
-__d("legacy:PageFanning",["PageFanning"],function(a,b,c,d){a.PageFanning=b('PageFanning');},3);
