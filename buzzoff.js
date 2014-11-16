9eab375cffbaf2220bbb855f3e885267121d3f94
tamblc <tamblc@rpi.edu>
2014-11-15 22:19:41 -0500

    Updated so it works
    yay!!!!

--- buzzoff.js
+++ buzzoff.js
@@ -1,54 +1,56 @@
 function buzzoff(){
 
-	var stories = document.getElementsByClassName("_4-u2 mbm _5jmm _5pat _5v3q _5x16 _x72");
-	//alert(stories);
+	var stories = document.querySelectorAll("._4-u2");
 	var story;
+	alert(stories.length);
 	for(var i=0; i < stories.length; i++){
 		story = stories[i];
-		killStories(story);
+		if (story.value.toString().indexOf("buzzfeed.com") !== -1){
+			killStories(story);
+		}
 	}
 
-	stories = document.getElementsByClassName("_4-u2 mbm _5jmm _5pat _5v3q _5x16");
-	//alert(stories);
-	for(var j=0; i < stories.length; i++){
-		story = stories[j];
-		killStories(story);
-	}
+	// stories = document.getElementsByClassName("_4-u2 mbm _5jmm _5pat _5v3q _5x16");
+	// //alert(stories);
+	// for(var j=0; i < stories.length; i++){
+	// 	story = stories[j];
+	// 	killStories(story);
+	// }
 
-	stories = document.getElementsByClassName("timelineUnitContainer pagesTimelinePinpost");
-	//alert(stories);
-	//alert(stories[0].toString());
-	for(var k=0; i < stories.length; i++){
-		story = stories[k];
-		killStories(story);
-	}
+	// stories = document.getElementsByClassName("timelineUnitContainer pagesTimelinePinpost");
+	// //alert(stories);
+	// //alert(stories[0].toString());
+	// for(var k=0; i < stories.length; i++){
+	// 	story = stories[k];
+	// 	killStories(story);
+	// }
 	
-	stories = document.getElementsByClassName("timelineUnitContainer");
-	//alert(stories);
-	//alert(stories[0].toString());
-	for(var l=0; i < stories.length; i++){
-		story = stories[l];
-		killStories(story);
-	}
+	// stories = document.getElementsByClassName("timelineUnitContainer");
+	// //alert(stories);
+	// //alert(stories[0].toString());
+	// for(var l=0; i < stories.length; i++){
+	// 	story = stories[l];
+	// 	killStories(story);
+	// }
 
 }
 
 function killStories(item){
 
-	var elems = item.getElementsByClassName("fsm fwn fcg");
-
-	var post;
-	for(var i=0; i < elems.length; i++){
-	    post = elems[i];
-	    alert(post.innerHTML)
+	// var post;
+	// post = item;
+	// for(var i=0; i < elems.length; i++){
+	//     post = elems[i];
+	    alert(item.innerHTML);
 	    
-	    if (post.value.toString().indexOf("buzzfeed.com") !== -1){
+	   //if (item.value.toString().indexOf("buzzfeed.com") !== -1){
 	    	item.style.opacity = "0.0";
 	    	item.style.display = "None";
-    	}
+    	//}
     	
-	}
+	//}
 }
+
 
 buzzoff();
 
